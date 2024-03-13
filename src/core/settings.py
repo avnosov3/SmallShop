f5f7f8f88b81770772ec6883d1444e497f3ff694
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2024-03-13 22:09:03 +0100

    linters

--- src/core/settings.py
+++ src/core/settings.py
@@ -19,9 +19,9 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_IS_DEBUG", default=False)
 
-ALLOWED_HOSTS = tuple(allowed_host for allowed_host in env.list('DJANGO_ALLOWED_HOSTS'))
+ALLOWED_HOSTS = tuple(allowed_host for allowed_host in env.list("DJANGO_ALLOWED_HOSTS"))
 
-INTERNAL_IPS = tuple(allowed_host for allowed_host in env.list('DJANGO_ALLOWED_HOSTS'))
+INTERNAL_IPS = tuple(allowed_host for allowed_host in env.list("DJANGO_ALLOWED_HOSTS"))
 
 DJANGO_SUPER_ADMIN = env("DJANGO_SUPER_ADMIN")
 
@@ -31,87 +31,85 @@ DJANGO_SUPER_ADMIN_EMAIL = env("DJANGO_SUPER_ADMIN_EMAIL")
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'drf_yasg',
-    'rest_framework',
-    'django_celery_beat',
-    'django_object_actions',
-
-    'shop',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "drf_yasg",
+    "rest_framework",
+    "django_celery_beat",
+    "django_object_actions",
+    "shop",
 ]
 
 if DEBUG:
-    INSTALLED_APPS += ['debug_toolbar', 'django_extensions']
+    INSTALLED_APPS += ["debug_toolbar", "django_extensions"]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'NAME': env('DB_NAME'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "NAME": env("DB_NAME"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     },
 }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
@@ -119,116 +117,116 @@ USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # AWS S3
-AWS_S3_ENDPOINT_URL = env('MINIO_URL')
-AWS_S3_REGION_NAME = env('MINIO_REGION_NAME')
-AWS_S3_ACCESS_KEY_ID = env('MINIO_ROOT_USER')
-AWS_S3_SECRET_ACCESS_KEY = env('MINIO_ROOT_PASSWORD')
-AWS_STORAGE_BUCKET_NAME = env('MINIO_STATIC_BACKET_NAME')
-MINIO_MEDIA_BACKET_NAME = env('MINIO_MEDIA_BACKET_NAME') 
+AWS_S3_ENDPOINT_URL = env("MINIO_URL")
+AWS_S3_REGION_NAME = env("MINIO_REGION_NAME")
+AWS_S3_ACCESS_KEY_ID = env("MINIO_ROOT_USER")
+AWS_S3_SECRET_ACCESS_KEY = env("MINIO_ROOT_PASSWORD")
+AWS_STORAGE_BUCKET_NAME = env("MINIO_STATIC_BACKET_NAME")
+MINIO_MEDIA_BACKET_NAME = env("MINIO_MEDIA_BACKET_NAME")
 
 STORAGES = {
-    'default': {
-        'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-        'OPTIONS': {
-            'access_key': AWS_S3_ACCESS_KEY_ID,
-            'secret_key': AWS_S3_SECRET_ACCESS_KEY,
-            'bucket_name': MINIO_MEDIA_BACKET_NAME,
-            'endpoint_url': AWS_S3_ENDPOINT_URL,
-            'region_name': AWS_S3_REGION_NAME,
-            'url_protocol': 'http:',
-            'custom_domain': 'localhost:9000/media',
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "access_key": AWS_S3_ACCESS_KEY_ID,
+            "secret_key": AWS_S3_SECRET_ACCESS_KEY,
+            "bucket_name": MINIO_MEDIA_BACKET_NAME,
+            "endpoint_url": AWS_S3_ENDPOINT_URL,
+            "region_name": AWS_S3_REGION_NAME,
+            "url_protocol": "http:",
+            "custom_domain": "localhost:9000/media",
         },
     },
-    'staticfiles': {
-        'BACKEND': 'storages.backends.s3boto3.S3StaticStorage',
-        'OPTIONS': {
-            'access_key': AWS_S3_ACCESS_KEY_ID,
-            'secret_key': AWS_S3_SECRET_ACCESS_KEY,
-            'bucket_name': AWS_STORAGE_BUCKET_NAME,
-            'endpoint_url': AWS_S3_ENDPOINT_URL,
-            'region_name': AWS_S3_REGION_NAME,
-            'url_protocol': 'http:',
-            'custom_domain': 'localhost:9000/static',
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        "OPTIONS": {
+            "access_key": AWS_S3_ACCESS_KEY_ID,
+            "secret_key": AWS_S3_SECRET_ACCESS_KEY,
+            "bucket_name": AWS_STORAGE_BUCKET_NAME,
+            "endpoint_url": AWS_S3_ENDPOINT_URL,
+            "region_name": AWS_S3_REGION_NAME,
+            "url_protocol": "http:",
+            "custom_domain": "localhost:9000/static",
         },
     },
 }
 
 
 # Cache
-DJANGO_CACHE_TIME = env('DJANGO_CACHE_TIME')
-DEFAULT_CACHE = 'default'
+DJANGO_CACHE_TIME = env("DJANGO_CACHE_TIME")
+DEFAULT_CACHE = "default"
 
 CACHES = {
     DEFAULT_CACHE: {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': env('DJANGO_DEFAULT_CACHE_URL'),
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": env("DJANGO_DEFAULT_CACHE_URL"),
     }
 }
 
 # Celery
-CELERY_BROKER_URL = env('CELERY_BROKER_URL')
+CELERY_BROKER_URL = env("CELERY_BROKER_URL")
 CELERY_RETRY_ATTEMPTS = env("CELERY_RETRY_ATTEMPTS")
 CELERY_WAITING_TIME_BEFORE_NEW_ATTEMPTS = env("CELERY_WAITING_TIME_BEFORE_NEW_ATTEMPTS")
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = TIME_ZONE
 
 # Logs
 if DEBUG:
     LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'handlers': {
-            'console': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
             },
         },
-        'loggers': {
-            'django.db.backends': {
-                'handlers': ['console'],
-                'level': 'DEBUG',
+        "loggers": {
+            "django.db.backends": {
+                "handlers": ["console"],
+                "level": "DEBUG",
             },
         },
     }
 else:
     LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'verbose': {
-                'format': '{levelname} {asctime} {message}',
-                'style': '{',
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {message}",
+                "style": "{",
             },
         },
-        'handlers': {
-            'file': {
-                'level': 'INFO',
-                'class': 'logging.FileHandler',
-                'filename': 'django.log',
-                'formatter': 'verbose',
+        "handlers": {
+            "file": {
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "filename": "django.log",
+                "formatter": "verbose",
             },
-            'console': {
-                'level': 'INFO',
-                'class': 'logging.StreamHandler',
-                'formatter': 'verbose',
+            "console": {
+                "level": "INFO",
+                "class": "logging.StreamHandler",
+                "formatter": "verbose",
             },
         },
-        'root': {
-            'handlers': ['file', 'console'],
-            'level': 'DEBUG',
+        "root": {
+            "handlers": ["file", "console"],
+            "level": "DEBUG",
         },
     }
 
 # DRF
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
 }
