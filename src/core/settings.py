3abb67c1d2307fba4a38c15a0a70bcd7c752e73b
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2024-03-13 22:11:58 +0100

    linters

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,19 +30,25 @@ DJANGO_SUPER_ADMIN_PASSWORD = env("DJANGO_SUPER_ADMIN_PASSWORD")
 DJANGO_SUPER_ADMIN_EMAIL = env("DJANGO_SUPER_ADMIN_EMAIL")
 
 # Application definition
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+THIRD_PARTY_APPS = [
     "drf_yasg",
     "rest_framework",
     "django_celery_beat",
     "django_object_actions",
+]
+LOCAL_APPS = [
     "shop",
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 if DEBUG:
     INSTALLED_APPS += ["debug_toolbar", "django_extensions"]
@@ -156,7 +162,6 @@ STORAGES = {
         },
     },
 }
-
 
 # Cache
 DJANGO_CACHE_TIME = env("DJANGO_CACHE_TIME")
