e689a91effca7223cc2f02445b260e38b24d865d
Artem Nosov <103992767+avnosov3@users.noreply.github.com>
2024-03-13 22:15:52 +0100

    add swagger

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,17 +1,36 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("core.api_router")),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SmallShop",
+        default_version="v1",
+        description="Документация для приложения SmallShop",
+        contact=openapi.Contact(email="custom@me.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("api/v1/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
 
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
